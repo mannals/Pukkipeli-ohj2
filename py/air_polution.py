cd2be47d3862b4f2ebd73f7dd7e8cbb9337522cd
leivay <111729241+leivay@users.noreply.github.com>
2022-12-11 18:32:32 +0200

    air polution

--- py/air_polution.py
+++ py/air_polution.py
@@ -1,11 +1,15 @@
 import requests
 from airport import Lentokentta
+from flask import jsonify
+
 
 lentsi = Lentokentta()
 lentsikirja = lentsi.luo_lentokenttalista()
 
-lat = lentsikirja['latitude']
-lon = lentsikirja['longitude']
+lentsi_json = jsonify(lentsikirja)
+
+lat = lentsi_json['latitude']
+lon = lentsi_json['longitude']
 
 url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid=d8d27e31da98f04894338ef22df6fec9'
 
