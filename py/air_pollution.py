b135fce0240ce46956d38b715c5d1e967eee57c5
TiiVii <111729213+TiiVii@users.noreply.github.com>
2022-12-12 14:57:17 +0200

    Update air_pollution.py

--- py/air_pollution.py
+++ py/air_pollution.py
@@ -1,6 +1,10 @@
 from airport import Lentokentta
+from database import Tietokanta
 import json
 import requests
+
+lentopeli = Tietokanta()
+yhteys = lentopeli.ota_yhteys()
 
 lentsi = Lentokentta()
 lentsikirja = lentsi.luo_lentokenttalista()
@@ -13,6 +17,9 @@ for values in lentsikirjaParse.values():
     lon = values['longitude']
     url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid=df2f23555b8bb70eff10e105302daa5f'
     vastaus = requests.get(url).json()
-    index = vastaus['list'][0]['main']['aqi']
-    print(index)
+    index = int(vastaus['list'][0]['main']['aqi'])
 
+    def luo_aqi_index():
+        sql = f'UPDATE airport SET air_pollution = {index} WHERE latitude_deg = "{lat}" AND longitude_deg = "{lon}";'
+        kursori = yhteys.cursor()
+        kursori.execute(sql)
