130fe874be90140857b01d0b353b9477dee64e72
mannals <annamalassu@gmail.com>
2022-12-13 10:46:09 +0200

    ilmansaasteindeksit toimii

--- py/air_polution.py
+++ py/air_polution.py
@@ -5,17 +5,33 @@ from flask import jsonify
 
 lentsi = Lentokentta()
 lentsikirja = lentsi.luo_lentokenttalista()
-
-lentsi_json = jsonify(lentsikirja)
-
-lat = lentsi_json['latitude']
-lon = lentsi_json['longitude']
-
-url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid=d8d27e31da98f04894338ef22df6fec9'
-
-vastaus = requests.get(url).json()
+print(lentsikirja)
 
 
-index = vastaus['list']['main']['aqi']
+class Ilmansaasteet:
 
-print(index)
+    def __init__(self, lentokenttadata):
+        self.lentokenttadata = lentokenttadata
+    def lentsi_ja_indeksi(self):
+        indeksikirjasto = {}
+        for lentsi in self.lentokenttadata:
+            lat = self.lentokenttadata[lentsi]['latitude']
+            lon = self.lentokenttadata[lentsi]['longitude']
+
+            url = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid=1eaca74789ff332a01d1c16c5e890e3f'
+
+            vastaus = requests.get(url).json()
+            print(vastaus)
+
+            index = vastaus['list'][0]['main']['aqi']
+
+            indeksikirjasto[lentsi] = index
+
+        return indeksikirjasto
+
+
+saastedata = Ilmansaasteet(lentsikirja)
+lentsit_indeksit = saastedata.lentsi_ja_indeksi()
+
+
+
--- py/main.py
+++ py/main.py
@@ -1,4 +1,5 @@
 from airport import Lentokentta
+
 import json
 from flask import Flask, Response
 from flask_cors import CORS
@@ -7,7 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 lentsi = Lentokentta()
-lentsilista = lentsi.luo_lentokenttalista()
+lentsi.luo_lentokenttalista()
+lentsi.lentsi_ja_indeksi()
+lentsit = lentsi.lentokenttadata
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -15,8 +19,8 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/lentokentat/<lista>')
 def lentokentat(lista):
     try:
-        global lentsilista
-        lista = lentsilista
+        global lentsit
+        lista = lentsit
         lista_json = json.dumps(lista)
         return lista_json
     except ValueError:
@@ -27,6 +31,8 @@ def lentokentat(lista):
         json_vastaus = json.dumps(vastaus)
         http_vastaus = Response(response=json_vastaus, status=404, mimetype="application/json")
         return http_vastaus
+
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
