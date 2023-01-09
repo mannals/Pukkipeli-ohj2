dd82d93ffb3e08743fd67b5676ea1e53af6c2a13
TiiVii <111729213+TiiVii@users.noreply.github.com>
2023-01-09 18:36:24 +0200

    highscorejuttui

--- py/highscore.py
+++ py/highscore.py
@@ -4,20 +4,20 @@ import plotly.graph_objects as go
 import plotly.offline as po
 import mysql.connector
 
-sivu = 'C:/Users/OMISTAJA/OneDrive/Tiedostot/GitHub/Pukkipeli-ohj2/top_pisteet.html'
+sivu = 'top_pisteet.html'
 
 yhteys = mysql.connector.connect(
     host='localhost',
     port=3306,
     database='fl1ght_game',
     user='root',
-    password='MiksiRikoit56Lamppua?',
+    password='vaahtokarkki',
     autocommit=True
     )
 
 def tuo_pistedata():
     kursori = yhteys.cursor(dictionary=True, buffered=True)
-    sql = f"SELECT name, highscore FROM peli;"
+    sql = f"SELECT name, kakatut_kentat FROM peli;"
     kursori.execute(sql)
     tulos = kursori.fetchall()
     print(tulos)
@@ -34,15 +34,13 @@ def pistemaarat():
     pistelista = []
     tietolista = tuo_pistedata()
     for i in range(5):
-        pistelista.append(tietolista[i]['highscore'])
+        pistelista.append(tietolista[i]['kakatut_kentat'])
     return pistelista
 
 
 fig = go.Figure(data=[go.Table(header=dict(values=['Player', 'Highscore']),
                  cells=dict(values=[nimet(), pistemaarat()]))
                      ])
-fig.show()
-print(type(fig))
 
 divi = po.plot(fig, include_plotlyjs=False, output_type='div')
 
